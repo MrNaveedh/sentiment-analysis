3ea77cc5c91cc45cbb93a1d3b1f80217bbb76d54
Naveedhe <naveedh7575@gmail.com>
2024-02-14 10:58:46 +0530

    code commited

--- main.py
+++ main.py
@@ -1,42 +1,114 @@
-from fastapi import FastAPI
-from models import Review, session
+from fastapi import FastAPI,HTTPException
+from models import Topic, Comment, session
 
 app = FastAPI()
 
-
-@app.post("/create")
-async def create_review(topic: str, feedBack: str):
-    review = Review(topic=topic, feedBack=feedBack)
-    session.add(review)
+#/api/topic/post
+@app.post("/api/create_topic/{topic}")
+async def create_topic(topic: str):
+    topic = Topic(topic=topic)
+    session.add(topic)
     session.commit()
-    return {"Topic":review.topic,"Feedback":review.feedBack}
+    return {"Message":"Topic Added"}
 
-@app.get("/")
-async def get_all_topics():
-    topics_query = session.query(Review.topic)  # Selecting only the 'topic' column
-    return [topic[0] for topic in topics_query.all()]
+#/api/topic/get
+@app.get("/api/retrieve_all_topics")
+async def retrieve_all_topics():
+    all_topics = session.query(Topic.topic).all()
+    return [topic[0] for topic in all_topics]
 
-@app.get("/feedBack/{topic}")
-async def get_feedBack(topic: str):
-    review_query = session.query(Review).filter(Review.topic == topic).all()
-    return {"reviews": review_query}
+#/api/topic/:id/get
+@app.get("/api/specific_topic_retrieve/{id}")
+async def specific_topic(id:int):
+    specific_topic=session.query(Topic).filter(Topic.topic_id==id).first()
+    if not specific_topic:
+            raise HTTPException(status_code=404, detail="Topic not found")
+    return {f"Topic in ID-{id}":specific_topic.topic}
 
-
-@app.put("/update/{id}")
-async def update_feedBack(
-    id: int,
-    feedBack: str = ""
-):
-    review_query = session.query(Review).filter(Review.id==id).first()
-    if feedBack:
-        review_query.feedBack = feedBack
-    session.add(review_query)
+#/api/topic/:id/put
+@app.put("/api/specific_topic_update/{id}")
+async def specific_topic_update(id:int,topic:str):
+    topics_query=session.query(Topic).filter(Topic.topic_id==id).first()
+    if not topics_query:
+            raise HTTPException(status_code=404, detail="Topic not found")
+    if topic :
+        topics_query.topic=topic
+    session.add(topics_query)
     session.commit()
+    return {"Message":"Topic updated"}
 
-@app.delete("/delete/{topic}")
-async def delete_topic(topic:str):
-    review_query = session.query(Review).filter(Review.topic==topic).all() # Review object
-    for review in review_query:
-        session.delete(review)
+#/api/topic/:id/delete
+@app.delete("/api/specific_topic_delete/{id}")
+async def specific_topic_delete(id:int):
+    topic_to_delete=session.query(Topic).filter(Topic.topic_id==id).first()
+    if not topic_to_delete:
+            raise HTTPException(status_code=404, detail="Topic not found")
+    session.delete(topic_to_delete)
     session.commit()
-    return {"topic deleted": topic}
+    return {"Message":"Topic Deleted"}
+
+#/api/topic/:id/comment/post
+@app.post("/api/comment_specific_topic/{topic}/{comment}")
+async def comment_specific_topic(topic:str,comment:str):
+    topics_obj=session.query(Topic).filter(Topic.topic==topic).first()
+    if not topic_obj:
+            raise HTTPException(status_code=404, detail="Topic not found")
+    comment=Comment(comment=comment,topic=topics_obj)
+    session.add(comment)
+    session.commit()
+    return {"Message":"Comment added"}
+
+#/api/topic/:id/retrieve_comments_topic_id/get
+@app.get("/api/retrieve_comments_topic_id/{id}")
+async def retrieve_comments_topic_id(id:int):
+    all_comments=session.query(Comment).filter(Comment.topic_id==id).all()
+    if not all_comments:
+            raise HTTPException(status_code=404, detail="Topic not found")
+    return all_comments
+
+#/api/topic/:id/comment/:id/get
+@app.get("/api/retrieve_specific_topic_specific_comment/{topic_id}/{comment_id}")
+async def retrieve_specific_topic_specific_comment(topic_id:int,comment_id:int):
+    topic_obj=session.query(Topic).filter(Topic.topic_id==topic_id).first()
+    if not topic_obj:
+            raise HTTPException(status_code=404, detail="Topic_ID not found")
+    comment_obj=session.query(Comment).filter(Comment.comment_id==comment_id).first()
+    if not comment_obj:
+            raise HTTPException(status_code=404, detail="Comment_ID not found")
+    if not topic_id==comment_obj.topic_id:
+            raise HTTPException(status_code=404, detail="Topic_ID is not specific to the comment_ID")
+    return {topic_obj.topic:comment_obj.comment}
+
+#/api/topic/:id/comment/put
+@app.put("/api/update_specific_comment_specific_topic/{topic_id}/{comment_id}/{comment}")
+async def update_specific_comment_specific_topic(topic_id:int,comment_id:int,comment:str):
+    topic_obj=session.query(Topic).filter(Topic.topic_id==topic_id).first()
+    if not topic_obj:
+            raise HTTPException(status_code=404, detail="Topic_ID not found")
+    comment_obj=session.query(Comment).filter(Comment.comment_id==comment_id).first()
+    if not comment_obj:
+            raise HTTPException(status_code=404, detail="Comment_ID not found")
+    if not topic_id==comment_obj.topic_id:
+            raise HTTPException(status_code=404, detail="Topic_ID is not specific to the comment_ID")
+    comment_obj.comment=comment
+    session.add(comment_obj)
+    session.commit()
+    return {"Message":"Comment updated"}
+
+#/api/topic/:id/comment/delete
+@app.put("/api/delete_specific_comment_specific_topic/{topic_id}/{comment_id}")
+async def update_specific_comment_specific_topic(topic_id:int,comment_id:int):
+    topic_obj=session.query(Topic).filter(Topic.topic_id==topic_id).first()
+    if not topic_obj:
+            raise HTTPException(status_code=404, detail="Topic_ID not found")
+    comment_obj=session.query(Comment).filter(Comment.comment_id==comment_id).first()
+    if not comment_obj:
+            raise HTTPException(status_code=404, detail="Comment_ID not found")
+    if not topic_id==comment_obj.topic_id:
+            raise HTTPException(status_code=404, detail="Topic_ID is not specific to the comment_ID")
+    session.delete(comment_obj)
+    session.commit()
+    return {"Message":"Comment deleted"}
+
+
+
--- models.py
+++ models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import create_engine, Column, Integer, String, Boolean
+from sqlalchemy import create_engine, Column, Integer, String, Boolean, ForeignKey 
 from sqlalchemy.engine import URL
-from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
 
 engine = create_engine("sqlite:///mydb.db",echo=True)
@@ -9,12 +9,27 @@ session = Session()
 
 Base = declarative_base()
 
-class Review(Base):
-    __tablename__ = "review"
+class Topic(Base):
+    __tablename__ = "topic"
 
-    id = Column(Integer, primary_key=True)
+    topic_id = Column(Integer, primary_key=True)
     topic = Column(String)
-    feedBack = Column(String)
+    comment=relationship('Comment',backref='topic')
     
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    comment_id = Column(Integer, primary_key=True)
+    comment = Column(String)
+    topic_id = Column(Integer,ForeignKey('topic.topic_id'))    
+    emotions = relationship('Emotion', backref='comment')
+
+class Emotion(Base):
+    __tablename__ = "emotion"
+
+    emotion_id = Column(Integer, primary_key=True)
+    emotion_type = Column(String)
+    comment_id = Column(Integer, ForeignKey('comment.comment_id'))
 
 Base.metadata.create_all(bind=engine)
